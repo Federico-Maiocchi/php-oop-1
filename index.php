bed347b5289750c8d139bb1469785cde9a400d7a
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-11-28 15:04:21 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+// creazione della classe
+class Production {
+    public $title;
+    public $language;
+    public $rating;
+}
+//Creazione delle istanze della classe
+$movie_1 = new Production();
+$movie1->title = 'Il signore degli anelli';
+$movie1->language = 'En';
+$movie1->rating = 10;
+
+var_dump($movie_1);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>opp</title>
+</head>
+<body>
+    
+</body>
+</html>
