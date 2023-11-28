b263571253f41fc45184b185d81f8fb711a59102
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-11-28 15:15:11 +0100

    ADD: var_dump movie obj

--- index.php
+++ index.php
@@ -2,17 +2,30 @@
 
 // creazione della classe
 class Production {
+
     public $title;
     public $language;
     public $rating;
+
+    
 }
 //Creazione delle istanze della classe
 $movie_1 = new Production();
-$movie1->title = 'Il signore degli anelli';
-$movie1->language = 'En';
-$movie1->rating = 10;
+//Assegnazione valore alle istanze
+$movie_1->title = 'Il signore degli anelli';
+$movie_1->language = 'Eng';
+$movie_1->rating = 10;
 
 var_dump($movie_1);
+// var_dump('questo film si chiama'. ' ' . $movie_1->title);
+
+$movie_2 = new Production;
+$movie_2->title = 'Marco baldo';
+$movie_2->language = 'Ita';
+$movie_2->rating = 3;
+
+var_dump($movie_2);
+
 
 ?>
 
